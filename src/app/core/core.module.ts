5313a19dda077b58c31064d11d40dd727862e8ba
Tiago Fernandes <tiagowhite@gmail.com>
2019-11-24 22:33:29 -0300

    Strange things on material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,20 +2,23 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MaterialModule } from '../material.module';
 import { EnsureModuleLoadedOnceGuard } from '@core/ensureModuleLoadedOnceGuard';
+
+
 import * as core from '../core/components';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [...core.components],
   imports: [
     CommonModule,
     HttpClientModule,
-    MaterialModule
+    MatToolbarModule
   ],
   exports: [
-    ...core.components,
-    MaterialModule
+    ...core.components
   ]
 })
 export class CoreModule extends EnsureModuleLoadedOnceGuard {
